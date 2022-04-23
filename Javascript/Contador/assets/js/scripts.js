b6dd6260772438cfd1b8eb76ac6756481737ba1c
GeracNeto <gera.cost18@gmail.com>
2022-04-22 21:00:16 -0300

    Criado script para incrementar e decrementar valores ao clicar nos botões, nesse primeiro momomento o código não está funcionando

--- Javascript/Contador/assets/js/scripts.js
+++ Javascript/Contador/assets/js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello Word")
+console.log("Primeiro Projeto em Javascript")
+
+var currentnumberwrapper = document.getElementById('currentnumber')
+
+function increment(){
+    currentnumber = currentnumber + 1;
+    currentnumberwrapper.innerHTML = currentnumber;
+}
+
+function decrement(){
+    currentnumber = currentnumber - 1;
+    currentnumberwrapper.innerHTML = currentnumber;
+}
