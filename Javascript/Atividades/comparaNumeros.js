39b23d3b5b97ef87464e25234e1e936a30ee3aaf
GeracNeto <gera.cost18@gmail.com>
2022-04-23 11:29:10 -0300

    Criado função que verifica se os numeros são iguais, faz a soma deles e verifica se a soma é menor que 10 e 20

--- Javascript/Atividades/comparaNumeros.js
+++ Javascript/Atividades/comparaNumeros.js
@@ -1,1 +1,12 @@
+/* Atividade 1 - Compara Números */
 
+function comparaNumeros(num1, num2){
+    let compara = num1 === num2 ? "" : "não";
+    let soma = num1 + num2;
+    let compara10 = soma < 10 ? "menor" : "maior";
+    let compara20 = soma < 20 ? "menor" : "maior";
+
+    return console.log(`Os numeros ${num1} e ${num2} ${compara} são iguais. Sua soma é ${soma}, que é ${compara10} que 10 e ${compara20} que 20`);
+}
+
+comparaNumeros(11, 9);
