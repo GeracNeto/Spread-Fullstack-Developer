d88dc5c75521bee1c164fe14ba368a1c32b6bb51
GeracNeto <gera.cost18@gmail.com>
2022-04-27 21:05:31 -0300

    Desenvolvido um segundo método para verificar se a palavra é um palíndromo

--- Javascript/Atividades/palindromo.js
+++ Javascript/Atividades/palindromo.js
@@ -16,4 +16,28 @@ function verificaPalindromo(texto){
 
 verificaPalindromo("ovo");
 
+/**********************************************************************************************************/
+
 //Método 2
+console.log("Método ")
+let texto2 = "rever";
+
+
+function verificaPalindromo2(text2){
+    let array2 = texto2.split("");
+    let equal = [];
+
+    for(let i = 0; i < array2.length; i++){
+        equal.push(array2[i] === texto2.split("").reverse()[i]);
+   }
+
+   if(!equal.includes(false)){
+       return console.log(`A palavra ${texto2} é um palíndromo`);
+   }
+   else{
+       return console.log(`A palavra ${texto2} não é um palíndromo`);
+   }
+}
+
+
+verificaPalindromo2(texto2);
