e1d75f8bced8b279caf794d3dff1952b952cfbcf
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-05-22 14:37:52 -0300

    Adicionado método call, apply e bind

--- Javascript/This.js
+++ Javascript/This.js
@@ -1,4 +1,4 @@
-//console.log("Teste this")
+// Uso simples do This 
 
 const person = {
     name: "Geraldo",
@@ -16,3 +16,57 @@ const person = {
 
 console.log(person.getName());
 console.log(person.getAge());
+
+// Método Call
+
+const pessoa = {
+    nome: "Thainá"
+}
+
+const animal = {
+    nome: "Nika"
+}
+
+function getSomething(){
+    return console.log(this.nome);
+}
+
+getSomething.call(pessoa);
+getSomething.call(animal);
+
+// Call com argumentos
+
+const nums = {
+    num1: 1,
+    num2: 2
+}
+
+function soma(a,b){
+    return console.log(this.num1 + this.num2 + a + b);
+}
+
+soma.call(nums, 3, 4);
+
+// Método Apply
+
+const num = {
+    num1: 1,
+    num2: 2
+}
+
+function soma(a,b){
+    return console.log(this.num1 + this.num2 + a + b);
+}
+
+soma.apply(num, [3, 4]);
+
+// Método Bind
+
+const returnCity = function(){
+    return console.log(this.city);
+}
+
+const city1 = returnCity.bind({city: "Cachoeira de Minas"});
+
+city1();
+
