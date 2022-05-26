f4550f15093db1f8b46c6c39d7e3f6fb7955f137
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-05-26 19:54:45 -0300

    Atualizado com mais um exemplo do uso do método map

--- Javascript/Metodo-map.js
+++ Javascript/Metodo-map.js
@@ -1,9 +1,25 @@
 // Método map
 
+// O map retorna um novo array de acordo com a função inserida. Essa função vai passar por todos os elementos do array
+
 const array = [1, 2, 3, 4, 5];
 let newArray = [];
 
 newArray = array.map((item) => item * 2); // Retorna: [2, 4, 6, 8, 10]
 
 console.log(newArray);
+
+// Outro exemplo
+
+const nomes = ['Geraldo', 'Millena', 'Thaina', 'Thiago'];
+
+function somaPereira(arr){
+    return arr + ' Pereira';
+}
+
+let nomesPereira = nomes.map(somaPereira); // o .map vai executar a função "somaPereira" para cada elemento do array
+
+console.log(nomesPereira);
+
+
 
