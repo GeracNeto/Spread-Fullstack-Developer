1e39c3de058919a6b5dfffdd3ddf49695c1172c9
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-05-27 20:36:42 -0300

    Atualizado com mais um exemplo de aplicação do método reduce

--- Javascript/Atividades/sintaxeReduce.js
+++ Javascript/Atividades/sintaxeReduce.js
@@ -22,3 +22,46 @@ function somaArrayComInitial(arr, initialValue){
 
 console.log('Com o uso do initialValue: ' + somaArrayComInitial(numbers, 200)); // Como valor inicial está sendo passado 200. Logo a soma final terá 200 a mais
 
+// Soma dos elementos de um array sem o uso do initialValue
+
+/*
+
+Crie uma função que recebe uma lista de preços e um número
+representando o saldo disponível. Calcule qual será o saldo
+final após subtrair todos os preços da lista enviada
+
+*/
+
+
+const listaPrecos = [
+    {
+      produto: 'Mouse',
+      preco: 100.00
+    },
+    
+      {
+      produto: 'Teclado',
+      preco: 315.00
+    },
+    
+      {
+      produto: 'Monitor',
+      preco: 1415.00
+    },
+    
+      {
+      produto: 'MousePad',
+      preco: 75.00
+    }
+  ];
+  
+  function calculaSaldo(lista, carteira){
+    
+    return 'Saldo Final: ' + lista.reduce(function(total, currentValue){
+      
+      return total - currentValue.preco;
+                        
+    },carteira) 
+  }
+  
+  console.log(calculaSaldo(listaPrecos, 2000));
