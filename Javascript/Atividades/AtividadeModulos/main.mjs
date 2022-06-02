25ad0ad9feaf4371bfd73629179fc5fef107ebeb
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-02 20:59:20 -0300

    Criado main.mjs para importar as funções exportadas pelo arquivo funcoes.mjs

---
+++ Javascript/Atividades/AtividadeModulos/main.mjs
@@ -0,0 +1,8 @@
+// Import de funcoes.mjs
+
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs"; // Deve colocar o ./ antes do nome do arquivo
+
+console.log(mostraIdade('Geraldo', 23));
+console.log(mostraCidade('Geraldo', 'São José dos Campos'));
+console.log(mostraHobby('Geraldo', 'Jogar Vôlei'));
+
