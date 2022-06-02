198bf5f1af8d763b96be4f08f3817ae55d8b38ab
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-02 20:58:51 -0300

    Criado funções.mjs para colocar as funções que serão exportadas para o arquivo main.mjs

---
+++ Javascript/Atividades/AtividadeModulos/funcoes.mjs
@@ -0,0 +1,17 @@
+//Funções
+
+function mostraIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade){
+    return `A cidade de ${nome} é ${cidade}`;
+}
+
+function mostraHobby(nome, hobby){
+    return `O hobby de ${nome} é ${hobby}`;
+}
+
+// Export
+
+export{mostraIdade, mostraCidade, mostraHobby};
