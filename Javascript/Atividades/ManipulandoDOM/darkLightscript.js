3ca07747d3f88825101e0c38dbf913cc5f2c5c78
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-04 04:54:47 -0300

    Adicionado função para mudar a imagem ao clicar no botão

--- Javascript/Atividades/ManipulandoDOM/darkLightscript.js
+++ Javascript/Atividades/ManipulandoDOM/darkLightscript.js
@@ -3,6 +3,7 @@
 // Cria as constantes para capturar os elementos que serão alterados no HTML
 const darkModeButton = document.getElementById("buttonMode");
 const darkModeBody = document.querySelector('body');
+const darkModeImg = document.getElementById('sunmoon');
 
 // Aguarda o evento de click no botão para executar a função changeMode
 darkModeButton.addEventListener("click", changeMode);
@@ -19,12 +20,21 @@ function changeText(){
         darkModeButton.innerHTML = 'Light Mode';
         return; // Retorna para poder sair da função
     }
-
     darkModeButton.innerHTML = 'Dark mode'; // Caso não faça nada mantém o estado inicial que é Dark Mode
+}
+
+// Muda o atributo src trocando o caminho da imagem no HTML
+function changeImg(){
+    if(darkModeBody.classList.contains('dark-mode')){
+        darkModeImg.setAttribute('src', 'moon.png');
+        return;
+    }
+    darkModeImg.setAttribute('src', 'sun.png');    
 }
 
 // Chama as duas funções para executar a alterações assim que o botão é clicado
 function changeMode(){
     changeClasses();
     changeText();
+    changeImg();
 }
