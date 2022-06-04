80965af6ec31dd459cb94254ad89bcaed4a66d21
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-04 03:56:50 -0300

    Organizado o código separando as funções

--- Javascript/Atividades/ManipulandoDOM/darkLightscript.js
+++ Javascript/Atividades/ManipulandoDOM/darkLightscript.js
@@ -1,21 +1,23 @@
 const darkModeButton = document.getElementById("buttonMode");
 const darkModeBody = document.querySelector('body');
 
-darkModeButton.addEventListener("click", changeBackground);
+darkModeButton.addEventListener("click", changeMode);
 
-function changeBackground(){
-
+function changeClasses(){
     darkModeButton.classList.toggle('dark-mode');
     darkModeBody.classList.toggle('dark-mode');
-    changeText();
-    console.log('Botão acionado');
 }
 
 function changeText(){
     if(darkModeBody.classList.contains('dark-mode')){
-        darkModeButton.innerHTML = 'Light-Mode';
+        darkModeButton.innerHTML = 'Light Mode';
         return;
     }
-    
-    darkModeButton.innerHTML = 'Dark-mode';
+
+    darkModeButton.innerHTML = 'Dark mode';
+}
+
+function changeMode(){
+    changeClasses();
+    changeText();
 }
