7ef428438685c2b5afa46116266ab1e64c2b59b6
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-04 04:01:11 -0300

    Adicionado comentários ao código

--- Javascript/Atividades/ManipulandoDOM/darkLightscript.js
+++ Javascript/Atividades/ManipulandoDOM/darkLightscript.js
@@ -1,22 +1,29 @@
+// As mudanças ocorridas ao clicar no botão estão na classe dark-mode que está no css
+
+// Cria as constantes para capturar os elementos que serão alterados no HTML
 const darkModeButton = document.getElementById("buttonMode");
 const darkModeBody = document.querySelector('body');
 
+// Aguarda o evento de click no botão para executar a função changeMode
 darkModeButton.addEventListener("click", changeMode);
 
+// Coloca nos elementos a classe dark-mode
 function changeClasses(){
     darkModeButton.classList.toggle('dark-mode');
     darkModeBody.classList.toggle('dark-mode');
 }
 
+// Altera o texto do botão
 function changeText(){
     if(darkModeBody.classList.contains('dark-mode')){
         darkModeButton.innerHTML = 'Light Mode';
-        return;
+        return; // Retorna para poder sair da função
     }
 
-    darkModeButton.innerHTML = 'Dark mode';
+    darkModeButton.innerHTML = 'Dark mode'; // Caso não faça nada mantém o estado inicial que é Dark Mode
 }
 
+// Chama as duas funções para executar a alterações assim que o botão é clicado
 function changeMode(){
     changeClasses();
     changeText();
