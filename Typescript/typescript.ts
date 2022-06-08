7f52aebcc60b34853258a244b6259774521b0a1b
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-06-08 19:05:24 -0300

    Criado interface ICaninos e objeto animall, criado type para unir IFelino ou ICanino

--- Typescript/typescript.ts
+++ Typescript/typescript.ts
@@ -1,14 +1,33 @@
 // Interfaces
+
 console.log('Interfaces');
 
 interface IAnimal{
     nome: string;
     tipo: 'Terrestre' | 'Aquatico';
+    domestico: boolean;
     emitirSom(typeSom: string): void; 
 }
 
 interface IFelino extends IAnimal{
     visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal{
+    porte: 'Grande' | 'Pequeno';
+}
+
+// Types
+
+type IDomestico = IFelino | ICanino; // Na hora de declarar o objeto posso "unir / juntar" as duas classes pelo type
+
+// Objetos
+
+const animal: IDomestico = {
+    nome: 'Peixe-Dourado',
+    tipo: 'Aquatico',
+    domestico: true, // Essa eu tbm não sou obrigado a passar
+    //visaoNoturna -> Essa propriedade eu posso ou não passar, pois coloquei IFelino "|" ICanino no type.
 }
 
 const animal1: IAnimal = {
@@ -23,6 +42,13 @@ const animal2: IFelino = {
     emitirSom: (typeSom) => (`O som é ${typeSom}`),
     visaoNoturna: true
 }
+
+
+
+
+
+
+// Testes
 
 console.log(animal2.emitirSom('Rugido'));
 console.log(animal1.nome);
